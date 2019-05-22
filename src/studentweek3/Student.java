7dd91964d081350ae5dc064f374e75ad53d3f93d
Andy <tran84@sheridancollege.ca>
2019-05-22 09:37:35 -0400

    Initial commit

---
+++ src/studentweek3/Student.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentweek3;
+
+/**
+ *
+ * @author andyt
+ */
+public class Student {
+    
+    private int id;
+    private String firstName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    
+}
